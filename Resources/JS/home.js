c4521bed695a9ac41beffaa7ae082f1d11e5e1bf
AlyssaBrockway <55685400+AlyssaBrockway@users.noreply.github.com>
2021-02-11 12:40:33 +0000

    Update home.js

--- Resources/JS/home.js
+++ Resources/JS/home.js
@@ -1,18 +1,20 @@
-if(document.id=="none"){};
-
-db.collection(movies).get.then(function (querysnapshot){
-    querysnapshot.forEach(async function(doc {
+function data(){
+    db.collection("movies").get.then(function (querysnapshot){
+        querysnapshot.forEach(async function(doc {
+            if (doc.id=="none") {}
+            else{        
+                container = document.createElement("div");
+                container.classList.add("container");
+                document.getElementById("films").appendChild(container);
                 
-        container = document.createElement("div");
-        container.classList.add("container");
-        document.getElementById("films").appendChild(container);
-        
-        image = document.createElement("img");
-        image.classList.add("poster");
-        document.container.appendChild(image);
+                image = document.createElement("img");
+                image.classList.add("poster");
+                document.container.appendChild(image);
 
-        title = document.createElement("h2");
-        title.classList.add("title");
+                title = document.createElement("h2");
+                title.classList.add("title");
 
-    }));
-});
+            }
+        }));
+    });
+};
