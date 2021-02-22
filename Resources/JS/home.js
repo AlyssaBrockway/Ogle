4aecea8ad228c433eae492222245cd139bb9513a
AlyssaBrockway <55685400+AlyssaBrockway@users.noreply.github.com>
2021-02-22 12:24:15 +0000

    added title, descp and rating

--- Resources/JS/home.js
+++ Resources/JS/home.js
@@ -1,42 +1,47 @@
-(function(){
-    db.collection("movies").get.then(function (querysnapshot){
+function addMovies(){
+    db.collection("movies").get().then(function (querysnapshot){
         querysnapshot.forEach(async function(doc) {
             if (doc.id=="none") {}
             else{
                 section = document.createElement("div");
                 section.classList.add("section");
-                document.getElementById("films").appendChild(section);
+                
 
                 container = document.createElement("div");
                 container.classList.add("container");
-                document.section.appendChild(container);
+                section.appendChild(container);
                 
                 image = document.createElement("img");
                 image.classList.add("poster");
-                document.container.appendChild(image);
+                
+                container.appendChild(image);
 
                 title = document.createElement("h2");
                 title.classList.add("title");
-                document.container.appendChild(title);
+                title.innerText = doc.data().title
+                container.appendChild(title);
 
                 description = document.createElement("p");
                 description.classList.add("description");
-                description.innerText = doc.data().title;
-                document.container.appendChild(description);
+                description.innerText = doc.data().description;
+                container.appendChild(description);
                 
                 rating = document.createElement("p");
                 rating.classList.add("rating");
-                document.container.appendChild(rating);
-
+                rating.innerText = doc.data().age;
+                container.appendChild(rating);
+                
                 times = document.createElement("div");
                 times.classList.add("times");
-                document.section.appendChild(times);
+                section.appendChild(times);
 
                 time = document.createElement("p");
-                document.times.appendChild(time);
-
+                times.appendChild(time);
+                
+                document.getElementById("films").appendChild(section);
 
             }
         });
     });
-})();
+};
+window.addEventListener("DOMContentLoaded", addMovies);
