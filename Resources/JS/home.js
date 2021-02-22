283ed3f16da29f0660d310824a7b5d75703ae3fd
AlyssaBrockway <55685400+AlyssaBrockway@users.noreply.github.com>
2021-02-22 10:41:00 +0000

    Update home.js

--- Resources/JS/home.js
+++ Resources/JS/home.js
@@ -2,10 +2,14 @@
     db.collection("movies").get.then(function (querysnapshot){
         querysnapshot.forEach(async function(doc) {
             if (doc.id=="none") {}
-            else{        
+            else{
+                section = document.createElement("div");
+                section.classList.add("section");
+                document.getElementById("films").appendChild(section);
+
                 container = document.createElement("div");
                 container.classList.add("container");
-                document.getElementById("films").appendChild(container);
+                document.section.appendChild(container);
                 
                 image = document.createElement("img");
                 image.classList.add("poster");
@@ -13,8 +17,25 @@
 
                 title = document.createElement("h2");
                 title.classList.add("title");
+                document.container.appendChild(title);
+
+                description = document.createElement("p");
+                description.classList.add("description");
+                document.container.appendChild(description);
+
+                rating = document.createElement("p");
+                rating.classList.add("rating");
+                document.container.appendChild(rating);
+
+                times = document.createElement("div");
+                times.classList.add("times");
+                document.section.appendChild(times);
+
+                time = document.createElement("p");
+                document.times.appendChild(time);
+
 
             }
         });
     });
-});
+})();
